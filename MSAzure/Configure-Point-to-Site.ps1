6314b1b8ae641fc22c1705f4267ea118c825eb20
prem <premkumar.unix@gmail.com>
2020-06-08 09:34:20 +0530

    added azure materials

---
+++ MSAzure/Configure-Point-to-Site.ps1
@@ -0,0 +1,27 @@
+
+$vpnClientPool = "10.1.0.0/24"
+# Below is your First VNET Gateway
+$vpnGWName = "DemoVNetGW1"
+# Below is Resource Group of your MYGW1
+$rgName = "VNetDemoRG"
+$rootCertName = "P2SRoot.cer" 
+
+$publicCertData = "<Paste-PublicKey-Here>" 
+
+# Retrieve the current virtual network configuration 
+$vpnGW = Get-AzureRmVirtualNetworkGateway -ResourceGroupName $rgName `
+                                          -Name $vpnGWName
+
+
+# This cmdlet adds the client address pool defined in the $vpnClientPool variable to the gateway
+# This is the range of addresses VPN clients will use to connect to the gateway
+Set-AzureRmVirtualNetworkGatewayVpnClientConfig `
+             -VirtualNetworkGateway $vpnGW `
+             -VpnClientAddressPool $vpnClientPool  
+
+# This cmdlet applies the certificate to the gateway for authentication
+Add-AzureRmVpnClientRootCertificate `
+            -VpnClientRootCertificateName $rootCertName `
+            -VirtualNetworkGatewayName $vpnGWName `
+            -ResourceGroupName $rgName `
+            -PublicCertData $publicCertData
